906da8c3a03be81f69b0219eb07cc2f23daaf226
tesac <tesac@DESKTOP-6UNM79G>
2021-02-19 10:36:52 +0100

    First commit

--- FirstTestingApp/Form1.vb
+++ FirstTestingApp/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Debug.WriteLine("PPP OOO")
+        Debug.WriteLine("PPP OOO PPP")
+        Debug.WriteLine("Other line added")
     End Sub
 End Class
